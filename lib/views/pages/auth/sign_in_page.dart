a87192c3c212278a4245cca162c23885e62eae13
Felipe Bueno <bueno.felipe@gmail.com>
2023-07-10 17:37:47 -0300

    Login with Magic Link on desktop

--- lib/views/pages/auth/sign_in_page.dart
+++ lib/views/pages/auth/sign_in_page.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:stacker_news/data/sn_api.dart';
 import 'package:stacker_news/main.dart';
+import 'package:stacker_news/utils.dart';
 import 'package:stacker_news/views/pages/auth/check_email_page.dart';
+import 'package:stacker_news/views/pages/home_page.dart';
 import 'package:stacker_news/views/widgets/generic_page_scaffold.dart';
 
 class SignInPage extends StatelessWidget {
@@ -72,12 +76,53 @@ class _LoginFormState extends State<LoginForm> {
                     .requestMagicLink(_emailController.text);
                 _busy.value = false;
 
-                if (ret && context.mounted) {
+                if (ret &&
+                    (Platform.isAndroid || Platform.isIOS) &&
+                    context.mounted) {
                   Navigator.pushNamed(context, CheckEmailPage.id);
                 }
               },
               child: const Text('Request Magic Link'),
             ),
+            if (!Platform.isAndroid && !Platform.isIOS)
+              TextField(
+                controller: _tokenController,
+                decoration: const InputDecoration(
+                  labelText: 'Magic Link',
+                  hintText: 'https://stacker.news/api/auth/callback/email/...',
+                ),
+              ),
+            if (!Platform.isAndroid && !Platform.isIOS)
+              const SizedBox(height: 24),
+            if (!Platform.isAndroid && !Platform.isIOS)
+              ElevatedButton(
+                onPressed: () async {
+                  if (_tokenController.text.isEmpty) {
+                    return;
+                  }
+
+                  try {
+                    _busy.value = true;
+                    final session =
+                        await locator<Api>().login(_tokenController.text);
+
+                    if (session != null) {
+                      if (context.mounted) {
+                        Navigator.pushReplacementNamed(context, HomePage.id);
+                      } else {
+                        Utils.showError(
+                          'Error going to home page. Context is not mounted',
+                        );
+                      }
+                    }
+                  } catch (e) {
+                    Utils.showError('Error logging in: ${e.toString()}');
+                  } finally {
+                    _busy.value = false;
+                  }
+                },
+                child: const Text('Login'),
+              ),
           ],
         ),
         ValueListenableBuilder<bool>(
